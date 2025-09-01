432fdc3bac8604cd616836f1ed2be4933a360086
Sergei Finogenov <serfinogenov@yandex.ru>
2025-09-01 22:31:42 +0300

    test ci/cd

--- config/settings.py
+++ config/settings.py
@@ -31,17 +31,17 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = ["89.169.163.110", "127.0.0.1", "localhost", "host.docker.internal"]
+ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://89.169.163.110:8000",
-    "http://127.0.0.1:80",
-    "http://127.0.0.1",
-    "http://127.0.0.1:8000",
-]
-
-CORS_ALLOWED_ORIGINS = ["*"]
-CORS_ALLOW_CREDENTIALS = True
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://89.169.163.110:8000",
+#     "http://127.0.0.1:80",
+#     "http://127.0.0.1",
+#     "http://127.0.0.1:8000",
+# ]
+#
+# CORS_ALLOWED_ORIGINS = ["*"]
+# CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 

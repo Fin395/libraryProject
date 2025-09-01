93950a8704f62476cd5432df56d274d24bb96ad1
Sergei Finogenov <serfinogenov@yandex.ru>
2025-09-02 00:20:56 +0300

    test ci/cd

--- config/settings.py
+++ config/settings.py
@@ -33,15 +33,15 @@ DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = ["*"]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://89.169.163.110:8000",
-#     "http://127.0.0.1:80",
-#     "http://127.0.0.1",
-#     "http://127.0.0.1:8000",
-# ]
-#
-# CORS_ALLOWED_ORIGINS = ["*"]
-# CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://89.169.163.110:8000",
+    "http://127.0.0.1:80",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000",
+]
+
+CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_CREDENTIALS = True
 
 
 INSTALLED_APPS = [

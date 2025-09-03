7eacd27e9639eb12ff18d4e406bafec78e759bf1
Sergei Finogenov <serfinogenov@yandex.ru>
2025-09-04 00:12:36 +0300

    test ci/cd

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ["89.169.163.110", "127.0.0.1", "localhost", "host.docker.internal"]
 CSRF_TRUSTED_ORIGINS = [
     "http://89.169.163.110:8000",
     "http://127.0.0.1:80",

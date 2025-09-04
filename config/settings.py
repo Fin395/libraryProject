e54cd0ec1505671423dd4cb875a588f629f58c3e
Sergei Finogenov <serfinogenov@yandex.ru>
2025-09-05 01:27:17 +0300

    test ci/cd

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = ["89.169.163.110"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "http://89.169.163.110:8000",
     "http://127.0.0.1:80",
